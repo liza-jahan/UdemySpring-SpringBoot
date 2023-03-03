9004039bf26bd9be4961dd7f31fa023585f40a3b
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-03-03 15:36:43 +0600

    Add files via upload

---
+++ SpringbootQueryDSL/src/main/java/com/luv2code/spring/FirstSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootMvcApplication.class, args);
+	}
+
+}
