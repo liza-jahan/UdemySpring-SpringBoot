3a276910a82a14719b81afd6defd9eb97194fc14
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-03-08 03:10:38 +0600

    Add files via upload

---
+++ SpringSecurityPArt-1/src/main/java/com/securet/spring/SecuretAppApplication.java
@@ -0,0 +1,13 @@
+package com.securet.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuretAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuretAppApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityPArt-1/src/test/java/com/securet/spring/SecuretAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securet.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuretAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
